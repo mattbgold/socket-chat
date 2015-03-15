36d3fab7b11658253dd18de0566c8948c4022b52
Matt Gold <mattbgold@gmail.com>
2015-03-15 08:37:57 -0400

    Added ability to set port on command line

--- index.js
+++ index.js
@@ -2,21 +2,27 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var port = process.argv[2] || '3000';
+
 app.get('/chat', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket) {
     console.log('a user connected');
+    //TODO: emit user connected
+
     socket.on('disconnect', function() {
-      console.log('user disconnected');
+        console.log('user disconnected');
+        //TODO: emit user disconnected
     });
+
     socket.on('chat message', function(msg) {
-    console.log('message: ' + msg);
-    io.emit('chat message', msg);
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
     });
 });
 
-http.listen(3000, function() {
-    console.log('listening on *:3000');
+http.listen(parseInt(port), function() {
+    console.log('listening on *:' + port);
 });
